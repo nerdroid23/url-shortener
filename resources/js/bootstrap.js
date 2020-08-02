a9e575128f5081ed6673c166f53f13645caaa2dd
Joe <nerdroid23@gmail.com>
2020-08-02 04:36:14 -0500

    [wip] - frontend
    - setp up vue, tailwind and alpine

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,28 +1,37 @@
-window._ = require('lodash');
+import 'alpinejs';
+import Vue from 'vue';
+import axios from 'axios';
+
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
-window.axios = require('axios');
+window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// import Echo from 'laravel-echo';
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-// window.Pusher = require('pusher-js');
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+window.Vue = Vue;
+
+const app = new Vue({
+  el: '#app',
+});
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>I'm an example component!</h1>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('Component mounted.')
+  }
+}
+</script>
